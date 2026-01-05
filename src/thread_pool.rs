ddb4aaa285f46b8dd49f44e9b0d7f626f182801e
4rjunc <arjunmundamani@gmail.com>
2026-01-05 11:15:40 +0530

    feat: add ThreadPool base

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -12,3 +12,44 @@ struct Worker {
     id: usize,
     thread: Option<thread::JoinHandle<()>>,
 }
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            todo!()
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
