35f66e0305c337d79133cb18897f84b2f6842192
4rjunc <arjunmundamani@gmail.com>
2026-01-05 12:49:38 +0530

    feat: add basic execution of threads

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 pub mod thread_pool;
 
+use std::thread;
+use std::time::Duration;
+use thread_pool::ThreadPool;
+
 fn main() {
-    println!("Hello, world!");
+    let pool = ThreadPool::new(4);
+
+    for i in 0..8 {
+        pool.execute(move || {
+            println!("Job {} starting on {:?}", i, thread::current().id());
+            thread::sleep(Duration::from_millis(500));
+            println!("Job {} done", i);
+        });
+    }
+
+    println!("All jobs submitted, dropping pool");
 }
--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -18,7 +18,18 @@ impl ThreadPool {
         assert!(size > 0);
 
         let (sender, receiver) = mpsc::channel();
+
+        // Arc and Mutex is used because one channel receiver, multiple worker threads need to pull
+        // from it. Arc allow multiple owner share the same data. Mutex to only one thread can
+        // access data at a time
+
         let receiver = Arc::new(Mutex::new(receiver));
+
+        //Arc<Mutex<Receiver<Job>>>
+        // │    │      │
+        // │    │      └── The actual data
+        // │    └── Only one thread touches it at a time
+        // └── Multiple threads can hold a pointer to it
 
         let mut workers = Vec::with_capacity(size);
 
@@ -41,10 +52,35 @@ impl ThreadPool {
     }
 }
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
-            todo!()
+        let thread = thread::spawn(move || {
+            loop {
+                //let job = receiver
+                //    .lock() // block untill we get exclusive access
+                //    .unwrap() // handle panics
+                //    .recv() // method used to receive data from a communication channel or a network socket
+                //    .unwrap();
+
+                match receiver.lock().unwrap().recv() {
+                    Ok(job) => job(),
+                    Err(_) => break,
+                }
+
+                //job();
+            }
         });
 
         Worker {
